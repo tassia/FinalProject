db846ba0c42f54de44ceb7e42e42bfcc28ee9154
tassia <araujot@vaniercollege.qc.ca>
2025-04-28 09:29:45 -0400

    Create the showAllActivities method

--- Activity.java
+++ Activity.java
@@ -25,9 +25,8 @@ public class Activity
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public void showAllActivities()
     {
         // put your code here
-        return x + y;
     }
 }
